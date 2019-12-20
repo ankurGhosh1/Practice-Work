9505b888b136b612b4357ece0b0490bfc2929fc3
ankur-ghosh-vst-au4 <ankurcool.kumar@gmail.com>
2019-12-20 18:55:53 +0530

    new
    new practice

---
+++ practice/script.js
@@ -0,0 +1,79 @@
+function addition(){
+    var val , value;
+
+    val = parseInt(document.querySelector("#num1").value);
+    value = parseInt(document.querySelector("#num2").value);
+
+    var total = val + value;
+
+    console.log(total);
+    document.querySelector('h2').innerHTML = total ;
+    document.querySelector('h2').style.border = "3px solid #000000";
+    document.querySelector('h2').style.width = "50%";
+    document.querySelector('h2').style.margin = "0 auto";
+    document.querySelector('h2').style.textAlign = "center"
+
+}
+
+function substract(){
+    var val , value;
+
+    val = parseInt(document.querySelector("#num1").value);
+    value = parseInt(document.querySelector("#num2").value);
+
+    var total = val - value;
+
+    console.log(total);
+    document.querySelector('h2').innerHTML = total ;
+    document.querySelector('h2').style.border = "3px solid #000000";
+    document.querySelector('h2').style.width = "50%";
+    document.querySelector('h2').style.margin = "0 auto";
+    document.querySelector('h2').style.textAlign = "center"
+
+}
+
+function multiply(){
+    var val , value;
+
+    val = parseInt(document.querySelector("#num1").value);
+    value = parseInt(document.querySelector("#num2").value);
+
+    var total = val * value;
+
+    console.log(total);
+    document.querySelector('h2').innerHTML = total ;
+    document.querySelector('h2').style.border = "3px solid #000000";
+    document.querySelector('h2').style.width = "50%";
+    document.querySelector('h2').style.margin = "0 auto";
+    document.querySelector('h2').style.textAlign = "center"
+
+}
+
+function division(){
+    var val , value;
+
+    val = parseInt(document.querySelector("#num1").value);
+    value = parseInt(document.querySelector("#num2").value);
+
+    var total = val / value;
+
+    console.log(total);
+    document.querySelector('h2').innerHTML = total ;
+    document.querySelector('h2').style.border = "3px solid #000000";
+    document.querySelector('h2').style.width = "50%";
+    document.querySelector('h2').style.margin = "0 auto";
+    document.querySelector('h2').style.textAlign = "center"
+}
+
+
+var xhttp = new XMLHttpRequest(); var fulldata;
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        fulldata = JSON.parse(this.responseText);
+    }
+}
+
+xhttp.open("GET", "https://raw.githubusercontent.com/attainu-falcon/falcon-course-module/master/coding-challenges/data/books.json", true);
+xhttp.send();
+
